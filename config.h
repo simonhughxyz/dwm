244c485ad91dc9205862b3e47d753bc1c5ccd985
Simon H Moore <simon@simonhugh.xyz>
2021-11-06 01:46:52 +0000

    refactor[config.h]: set basic colors

--- config.h
+++ config.h
@@ -7,15 +7,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
+static const char col_black[]       = "#000000";
+static const char col_white[]       = "#ffffff";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_purple_light[]  = "#bb00ff";
+static const char col_purple_dark[]   = "#9000ff";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg                 bg                  border   */
+	[SchemeNorm] = { col_purple_light,  col_black,          col_black },
+	[SchemeSel]  = { col_white,         col_purple_dark,    col_purple_dark  },
 };
 
 /* tagging */
