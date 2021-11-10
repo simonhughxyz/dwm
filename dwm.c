4ae57db247fe9e434fad0a5f5ec8455ed5bff490
Simon H Moore <simon@simonhugh.xyz>
2021-11-10 18:57:04 +0000

    fix[patch]: add missing setcfacts function

--- dwm.c
+++ dwm.c
@@ -1521,6 +1521,24 @@ setlayout(const Arg *arg)
 		drawbar(selmon);
 }
 
+void
+setcfact(const Arg *arg) {
+	float f;
+	Client *c;
+
+	c = selmon->sel;
+
+	if(!arg || !c || !selmon->lt[selmon->sellt]->arrange)
+		return;
+	f = arg->f + c->cfact;
+	if(arg->f == 0.0)
+		f = 1.0;
+	else if(f < 0.25 || f > 4.0)
+		return;
+	c->cfact = f;
+	arrange(selmon);
+}
+
 /* arg > 1.0 will set mfact absolutely */
 void
 setmfact(const Arg *arg)
