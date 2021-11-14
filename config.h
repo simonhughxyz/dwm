f6ed960153c68fa83336382bb4fa6fccda9507ca
Simon H Moore <simon@simonhugh.xyz>
2021-11-14 21:34:33 +0000

    add[config.h]: TERMCMD macro to help launch terminal based applications

--- config.h
+++ config.h
@@ -74,6 +74,9 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+/* helper to launch terminal based application */
+#define TERMCMD(class, title, cmd) { .v = (const char*[]){ "/usr/local/bin/st", "-c", class, "-t", title, "-e", cmd, NULL } }
+
 /* Tag Manipulation Controls */
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                  KEY,      view,           {.ui = 1 << TAG} }, \
